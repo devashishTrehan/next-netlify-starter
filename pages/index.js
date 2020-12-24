693e4585a959cb67970cccc74f850c195bdd6944
devashishTrehan <75111490+devashishTrehan@users.noreply.github.com>
2020-12-24 11:28:40 +0530

    Update index.js
    update

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,8 @@ export default function Home() {
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
-    <p>Hello I am devashish</p>
         </p>
+        <p>Hello I am devashish</p>
       </main>
 
       <Footer />
